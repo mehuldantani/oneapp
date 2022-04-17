7236cb57fc5a54d6bdbebb30f6303ce688a14066
mehuldantani97@gmail.com <mehuldantani97@gmail.com>
2022-04-17 13:41:48 -0700

    added API documenation throug swagger

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+//get express app
 const express =  require('express');
+//get date format
 const format = require('date-format');
 
+//define express
 const app = express();
 
+//swagger api document relateds
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+//for swagger api documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//define port
 const PORT = process.env.PORT || 4000;
 
+//home page of the application
 app.get("/",(req,res)=>{
     res.status(200).send("<h1>Hello From Mehul Dantani</h1>")
 });
 
+//instagram route
 app.get("/api/v1/instagram",(res,resp)=>{
     const instajason = {
         username : "mehuldantani97",
@@ -21,6 +35,7 @@ app.get("/api/v1/instagram",(res,resp)=>{
 
 });
 
+//facebook route
 app.get("/api/v1/facebook",(res,resp)=>{
     const instajason = {
         username : "mehuldantani97@meditab.com",
@@ -33,6 +48,7 @@ app.get("/api/v1/facebook",(res,resp)=>{
 
 });
 
+//linkedin route
 app.get("/api/v1/linkedin",(res,resp)=>{
     const instajason = {
         username : "mehullinkedin@meditab.com",
@@ -45,11 +61,13 @@ app.get("/api/v1/linkedin",(res,resp)=>{
 
 });
 
+//else route
 app.get("/api/v1/:token",(req,resp)=>{
     console.log(req.params.token);
     resp.status(200).json({parameter: req.params.token});
 });
 
+//start application listening to defined port
 app.listen(PORT, () => {
     console.log('Server is running at $('+PORT+')');
 });
